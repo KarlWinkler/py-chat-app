c51b99495da19fed7eac04e135f7719e00f58863
anil-mawji <mawjjianil1@gmail.com>
2024-04-10 23:20:14 -0600

    more prints

--- src/client.py
+++ src/client.py
@@ -92,8 +92,8 @@ class Client():
 
 
     def connect_to_peers(self, info_hash: str, tracker_response: dict):
+        print("trying connection with: ", len(tracker_response["peers"]))
         for peer_info in tracker_response["peers"]:
-            print("trying connection with: ", peer_info["ip"])
             # Only attempt to connect with seeding peers
             if self.connected_peers.get(peer_info["peer id"]) or not peer_info["seeding"]:
                 continue
